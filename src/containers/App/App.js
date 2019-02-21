0ae82906b36e9734ab4e3a74f465bdf6572ce449
Eric Weissman <eric.weissman21@gmail.com>
2019-02-21 15:38:27 -0700

    Adds initial functionality for fetching from Artist Area

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,27 +4,25 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import '../../styles/index.scss'
 import * as actions from '../../actions/index.js'
-import { fetchArtists} from '../../thunks/fetchArtists'
+import { fetchArtists } from '../../thunks/fetchArtists'
 import Instructions from '../../components/Instructions/Instructions'
 import NotFound from '../../components/NotFound/NotFound'
 import Nav from '../Nav/Nav'
-
-const dhURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=robin schulz&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25`
-    const ghURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=michal menert&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25`
-    const fbURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=hermitude&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25https://cors-anywhere.herokuapp.com/`
-    const tURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=baauer&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25https://cors-anywhere.herokuapp.com/`
+import ArtistArea from '../ArtistArea/ArtistArea'
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        {/* <Instructions /> */}
         <Nav />
         <Switch>
-            <Route exact path='/' component={Instructions} />
-            
-            <Route component={NotFound} />
+          <Route exact path='/' component={Instructions} />
+          <Route path='/glitchHop' render={({ match }) => <ArtistArea match={match} />} />
+          <Route path='/deepHouse' render={({ match }) => <ArtistArea match={match} />} />
+          <Route path='/futureBass' render={({ match }) => <ArtistArea match={match} />} />
+          <Route path='/trap' render={({ match }) => <ArtistArea match={match} />} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     );
---
+++ src/containers/ArtistArea/ArtistArea.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import * as actions from '../../actions'
+import { fetchArtists } from '../../thunks/fetchArtists'
+import { connect } from 'react-redux'
+const dhURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=robin schulz&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25`
+const ghURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=michal menert&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25`
+const fbURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=hermitude&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25https://cors-anywhere.herokuapp.com/`
+const tURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=baauer&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25https://cors-anywhere.herokuapp.com/`
+
+class ArtistArea extends Component {
+  componentDidMount() {
+    switch (this.props.match.path) {
+      case '/glitchHop':
+        if (this.props.glitchHop.length === 0) {
+          this.props.fetchArtists(ghURL, actions.getGlitchHopSuccess)
+        }
+        break;
+      case '/deepHouse':
+        if (this.props.deepHouse.length === 0) {
+          this.props.fetchArtists(dhURL, actions.getDeepHouseSuccess)
+        }
+        break;
+      case '/trap':
+        if (this.props.trap.length === 0) {
+          this.props.fetchArtists(tURL, actions.getTrapSuccess)
+        }
+        break;
+      case '/futureBass':
+        if (this.props.futureBass.length === 0) {
+          this.props.fetchArtists(fbURL, actions.getFutureBassSuccess)
+        }
+        break;
+      default:
+        return null
+    }
+  }
+  render() {
+    return (
+      <div>
+        card area
+      </div>
+    )
+  }
+}
+
+export const mapStateToProps = (state) => ({
+  glitchHop: state.glitchHop,
+  trap: state.trap,
+  deepHouse: state.deepHouse,
+  futureBass: state.futureBass
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  fetchArtists: (url, actionToDispatch) => dispatch(fetchArtists(url, actionToDispatch))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArtistArea)
