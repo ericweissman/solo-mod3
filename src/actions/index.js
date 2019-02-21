bbcd9179b095dd3017af8e8cda2b7c39ac1ce2b2
Eric Weissman <eric.weissman21@gmail.com>
2019-02-21 09:44:13 -0700

    Adjusts actions post-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,3 @@
-export const getDeepHouse = (artists) => ({
-  type: 'GET_DEEP_HOUSE',
-  artists
-})
-
-export const getFutureBass = (artists) => ({
-  type: 'GET_FUTURE_BASS',
-  artists
-})
-
-export const getGlitchHop = (artists) => ({
-  type: 'GET_GLITCH_HOP',
-  artists
-})
-
-export const getTrap = (artists) => ({
-  type: 'GET_TRAP',
-  artists
-})
-
 export const isLoading = (bool) => ({
   type: 'IS_LOADING',
   isLoading: bool
@@ -27,3 +7,29 @@ export const hasErrored = (error) => ({
   type: 'HAS_ERRORED',
   error
 })
+
+export const fetchDataSuccess = (artists) => ({
+  type: 'FETCH_DATA_SUCCESS',
+  artists
+})
+
+// export const getDeepHouse = (artists) => ({
+//   type: 'GET_DEEP_HOUSE',
+//   artists
+// })
+
+// export const getFutureBass = (artists) => ({
+//   type: 'GET_FUTURE_BASS',
+//   artists
+// })
+
+// export const getGlitchHop = (artists) => ({
+//   type: 'GET_GLITCH_HOP',
+//   artists
+// })
+
+// export const getTrap = (artists) => ({
+//   type: 'GET_TRAP',
+//   artists
+// })
+
