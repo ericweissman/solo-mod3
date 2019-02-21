496025a05022f7230170d4105e0bf3a8cbdbc269
Eric Weissman <eric.weissman21@gmail.com>
2019-02-20 17:07:53 -0700

    Adds actions

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+export const getDeepHouse = (artists) => ({
+  type: 'GET_DEEP_HOUSE',
+  artists
+})
+
+export const getFutureBass = (artists) => ({
+  type: 'GET_FUTURE_BASS',
+  artists
+})
+
+export const getGlitchHop = (artists) => ({
+  type: 'GET_GLITCH_HOP',
+  artists
+})
+
+export const getTrap = (artists) => ({
+  type: 'GET_TRAP',
+  artists
+})
+
+export const isLoading = (bool) => ({
+  type: 'IS_LOADING',
+  bool
+})
+
+export const hasErrored = (error) => ({
+  type: 'HAS_ERRORED',
+  error
+})
