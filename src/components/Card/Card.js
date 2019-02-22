14587ad601f984a1228decdd12b052ebe0593b5d
Eric Weissman <eric.weissman21@gmail.com>
2019-02-22 07:58:10 -0700

    Adds package to solve YouTube playback error

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import YouTube from 'react-youtube';
 
 const Card = ({name, video, wiki}) => {
   return(
     <div>
       <h4>{name}</h4>
       <p>{video}</p>
-      <p>{wiki}</p>
+      <YouTube 
+        videoId={video}
+        title={name}
+      />
+      <a href=`${wiki}`>
     </div>
   )
 }
