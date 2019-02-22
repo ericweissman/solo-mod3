41a8ba39cbd940a0cf181268fcf6f534579d565a
Eric Weissman <eric.weissman21@gmail.com>
2019-02-22 08:22:02 -0700

    Cleans up Artist Area and adds link to Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import YouTube from 'react-youtube';
 
-const Card = ({name, video, wiki}) => {
-  return(
+const Card = ({ name, video, wiki }) => {
+  return (
     <div>
       <h4>{name}</h4>
-      <p>{video}</p>
-      <YouTube 
+      <YouTube
         videoId={video}
         title={name}
       />
-      <a href=`${wiki}`>
+      <a href={wiki} target='blank'>More Info</a>
     </div>
   )
 }
--- src/containers/ArtistArea/ArtistArea.js
+++ src/containers/ArtistArea/ArtistArea.js
@@ -3,36 +3,31 @@ import * as actions from '../../actions'
 import { fetchArtists } from '../../thunks/fetchArtists'
 import { connect } from 'react-redux'
 import Card from '../../components/Card/Card'
-const keys = require('short-id') 
-
-
-const dhURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=robin schulz&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25`
-const ghURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=michal menert&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25`
-const fbURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=hermitude&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25`
-const tURL = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=baauer&k=330926-musicly-0PJ7J0DT&type=band&verbose=1&info=1&limit=25`
+import * as url from '../../utils/urls'
+const keys = require('short-id')
 
 class ArtistArea extends Component {
-  
+
   getArtists = () => {
     switch (this.props.match.path) {
       case '/glitchHop':
         if (this.props.glitchHop.length === 0) {
-          this.props.fetchArtists(ghURL, actions.getGlitchHopSuccess)
+          this.props.fetchArtists(url.glitchHop, actions.getGlitchHopSuccess)
         }
         return 'glitchHop'
       case '/deepHouse':
         if (this.props.deepHouse.length === 0) {
-          this.props.fetchArtists(dhURL, actions.getDeepHouseSuccess)
+          this.props.fetchArtists(url.deepHouse, actions.getDeepHouseSuccess)
         }
         return 'deepHouse'
       case '/trap':
         if (this.props.trap.length === 0) {
-          this.props.fetchArtists(tURL, actions.getTrapSuccess)
+          this.props.fetchArtists(url.trap, actions.getTrapSuccess)
         }
         return 'trap'
       case '/futureBass':
         if (this.props.futureBass.length === 0) {
-          this.props.fetchArtists(fbURL, actions.getFutureBassSuccess)
+          this.props.fetchArtists(url.futureBass, actions.getFutureBassSuccess)
         }
         return 'futureBass'
       default:
@@ -42,9 +37,8 @@ class ArtistArea extends Component {
 
   cardsToDisplay = () => {
     const genre = this.getArtists()
-    console.log(this.props[genre])
     const cards = this.props[genre].map(artist => {
-      return <Card {...artist} key={keys.generate()}/>
+      return <Card {...artist} key={keys.generate()} />
     })
     return cards
   }
