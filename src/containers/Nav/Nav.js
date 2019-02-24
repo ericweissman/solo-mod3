eb440195c9ba6162ce13d68ea4c3b69fe5926fb7
Eric Weissman <eric.weissman21@gmail.com>
2019-02-24 14:49:00 -0700

    Initial styles for Nav

--- src/containers/Nav/Nav.js
+++ src/containers/Nav/Nav.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+
 const Nav = () => {
   return (
     <nav>
       <h1>AARPlur</h1>
-      <NavLink to='/'> Instructions </NavLink>
-      <NavLink to='/glitchHop'> Glitch Hop </NavLink>
-      <NavLink to='/futureBass'> Future Bass </NavLink>
-      <NavLink to='/deepHouse'> Deep House </NavLink>
-      <NavLink to='/trap'> Trap </NavLink>
-      <NavLink to='/favorites'> Favorites </NavLink>
+      <div className='nav-options'>
+        <NavLink to='/' className='instructions-nav'> Instructions </NavLink>
+        <NavLink to='/glitchHop'> Glitch Hop </NavLink>
+        <NavLink to='/futureBass'> Future Bass </NavLink>
+        <NavLink to='/deepHouse'> Deep House </NavLink>
+        <NavLink to='/trap'> Trap </NavLink>
+        <NavLink to='/favorites'> Favorites </NavLink>
+      </div>
     </nav>
   )
 }
