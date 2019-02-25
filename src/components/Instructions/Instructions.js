503fe931857b2b544da466f1c7ba524601db94bf
Eric Weissman <eric.weissman21@gmail.com>
2019-02-24 18:22:26 -0700

    Adds initial styles for instructions

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -2,16 +2,15 @@ import React from 'react'
 
 const Instructions = () => {
   return (
-    <div>
-      <h3>Welcome to NAME OF APP</h3>
+    <div className='instructions'>
+      <h3>Welcome to AARPlur</h3>
       <p>Your one stop shop to learn about all the music that the young whippersnappers are boogying to!</p>
-      <h3>Instructions</h3>
-      <ol>
-        <li>Use your mouse to click one of the music genres on the menu bar above</li>
-        <li>Once the artists cards appear on the screen, you can use your mouse to click on parts of the card to get more info.</li>
+      <h5>Instructions</h5>
+      <ul>
+        <li>Select one of the music genres using the left-side menu</li>
         <li>If you click on the image on the card, it will play you a popular song from the artist. Make sure you have your volume turned on your computer!</li>
         <li>If you still want to learn more about the artist, click on the More Info button to visit their online encyclopedia page.</li>
-      </ol>
+      </ul>
       <p>We hope you enjoy learning about this newfangled robot music!</p>
     </div>
   )
