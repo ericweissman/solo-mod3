b37a27393603de83d673bed2accb24dac691ba60
Eric Weissman <eric.weissman21@gmail.com>
2019-02-24 18:22:13 -0700

    Adds initial styles for Error

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Error = (props) => {
   return (
-    <div>
+    <div className='error'>
       <h4>Dadgummit!</h4>
-      <p>Looks like the computer is on the fritz. Says here it's because: {props.message}!</p>
+      <p>Looks like the computer is on the fritz. Says here it's because:</p> 
+      <p className='error-message'>{props.message}!</p>
     </div>
   )
 }
