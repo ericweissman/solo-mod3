79871ca9fd3e705246cbeb4d488b9a5ca838a38d
Eric Weissman <eric.weissman21@gmail.com>
2019-02-26 16:01:00 -0700

    Adds styles for responive

--- src/components/FavoritesInstructions/FavoritesInstructions.js
+++ src/components/FavoritesInstructions/FavoritesInstructions.js
@@ -4,8 +4,7 @@ const FavoritesInstructions = () => {
   return (
     <div className='favoritesInstructions'>
       <h4>No Favorites Yet!</h4>
-      <p>To add a favorite, click on one of the music genres on the left and explore some of the artists.</p>
-      <p>If you like a particlar aritst, click the Favorite button and their info will be saved here for your next visit!</p>
+      <p>To add a favorite, click on one of the music genres on the left and explore some of the artists. If you like a particlar aritst, click the Favorite button and their info will be saved here for your next visit!</p>
     </div>
   )
 }
