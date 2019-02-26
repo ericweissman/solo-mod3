de02433ebed55eb0f6bb7501172a46252d0b158c
Eric Weissman <eric.weissman21@gmail.com>
2019-02-26 08:39:01 -0700

    Adds styles for FavoritesInstructions

--- src/components/FavoritesInstructions/FavoritesInstructions.js
+++ src/components/FavoritesInstructions/FavoritesInstructions.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const FavoritesInstructions = () => {
   return (
-    <div>
+    <div className='favoritesInstructions'>
       <h4>No Favorites Yet!</h4>
-      <p>To add a favorite, click on one of the music genres above and explore some of the artists. If you like a particlar aritst, click the Favorite button and their info will be saved here for your next visit!</p>
+      <p>To add a favorite, click on one of the music genres on the left and explore some of the artists. If you like a particlar aritst, click the Favorite button and their info will be saved here for your next visit!</p>
     </div>
   )
 }
