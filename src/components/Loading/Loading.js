24b65a95c34a20b9e93b0a1c4c6c44798ab614e3
Eric Weissman <eric.weissman21@gmail.com>
2019-02-26 08:39:25 -0700

    Adds styles for Loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Loading = () => {
   return(
-    <div>
+    <div className='loading'>
       <h4>Hold Your Horses!</h4>
-      <p>Whoa there, sport! Looks like the interwebs is still loading all these groovy tunes...</p>
+      <p className='notes'>Whoa there, sport! Looks like the interwebs is still loading all these groovy tunes...</p>
     </div>
   )
 }
